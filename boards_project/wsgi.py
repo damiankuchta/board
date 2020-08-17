bd67e0f0b8bbdf6f11bad9b22eed42f476cfabd1
pingwinoss <damkuch@gmail.com>
2020-08-17 01:42:04 +0100

    heroku

--- boards_project/wsgi.py
+++ boards_project/wsgi.py
@@ -11,7 +11,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'boards_project.settings.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'boards_project.settings.production')
 
 application = get_wsgi_application()
 
