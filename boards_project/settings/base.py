4de4214ee8e32aeee9b3c3308149f8dae30cec4e
pingwinoss <damkuch@gmail.com>
2020-08-17 01:17:15 +0100

    deployment

--- boards_project/settings/base.py
+++ boards_project/settings/base.py
@@ -138,6 +138,10 @@ EMAIL_HOST_USER=  "boards.app.dk@gmail.com"
 
 AUTH_USER_MODEL = 'userprofiles_app.User'
 
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
--- boards_project/settings/production.py
+++ boards_project/settings/production.py
@@ -1,4 +1,15 @@
+from boards_project.settings.base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 ALLOWED_HOSTS = [".herokuapp.com"]
+
+INSTALLED_APPS += \
+    ("whitenoise.runserver_nostatic",)
+
+MIDDLEWARE += \
+    ('whitenoise.middleware.WhiteNoiseMiddleware',
+     'reports_warrnings_bans_app.middleware.is_user_banned.IsUserBanned',
+     )
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
