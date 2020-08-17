d4a4b15b351c4f83b3ee3f93cb1acf4599eb19cd
pingwinoss <damkuch@gmail.com>
2020-08-17 03:09:17 +0100

    debug

--- boards_project/settings/production.py
+++ boards_project/settings/production.py
@@ -1,7 +1,7 @@
 from boards_project.settings.base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = [".herokuapp.com"]
 
 INSTALLED_APPS += \
